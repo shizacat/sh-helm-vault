10e7c1a9139d0ec200f13d356e477e9d4330bfd2
Alexey Matveev <tippet@yandex.ru>
2024-03-18 15:34:44 +0300

    config version setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='vault',
-   version='1.0.1',
+   version='1.2.0',
    description='Helm plugin for storing secrets in HashiCorp Vault',
    author='Just-Insane',
    author_email='justin@justin-tech.com',
